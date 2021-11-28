13ccca3c37c387a53d45ce5e49b925732b7091ee
Tijs van Roon <knifter@gmail.com>
2021-11-28 15:31:33 +0100

    Add user info str to debug funcs

--- src/soogh-debug.cpp
+++ src/soogh-debug.cpp
@@ -4,36 +4,40 @@ void debug_event_input(lv_event_t * e)
 {
 	lv_event_code_t code = lv_event_get_code(e);
     // lv_obj_t * obj = lv_event_get_target(e);
+	const char* info = "";
+	if(e->user_data)
+		info = (const char*) e->user_data;
+
 	switch(code)
 	{
 	    /** Input device events*/
-		case LV_EVENT_PRESSED: DBG("PRESSED"); return;
+		case LV_EVENT_PRESSED: DBG("%s: PRESSED", info); return;
 		// LV_EVENT_PRESSING
 		// LV_EVENT_PRESS_LOST
-		case LV_EVENT_SHORT_CLICKED: DBG("SHORT_CLICKED"); return;
-		case LV_EVENT_LONG_PRESSED: DBG("LONG_PRESSED"); return;
-		case LV_EVENT_LONG_PRESSED_REPEAT: DBG("LONG_PRESSED_REPEAT"); return;
-		case LV_EVENT_CLICKED: DBG("CLICKED"); return;
-		case LV_EVENT_RELEASED: DBG("RELEASED"); return;
+		case LV_EVENT_SHORT_CLICKED: DBG("%s: SHORT_CLICKED", info); return;
+		case LV_EVENT_LONG_PRESSED: DBG("%s: LONG_PRESSED", info); return;
+		case LV_EVENT_LONG_PRESSED_REPEAT: DBG("%s: LONG_PRESSED_REPEAT", info); return;
+		case LV_EVENT_CLICKED: DBG("%s: CLICKED", info); return;
+		case LV_EVENT_RELEASED: DBG("%s: RELEASED", info); return;
 		// LV_EVENT_SCROLL_BEGIN
 		// LV_EVENT_SCROLL_END
 		// LV_EVENT_SCROLL
 		// LV_EVENT_GESTURE
-		case LV_EVENT_KEY: DBG("KEY"); return;
-		case LV_EVENT_FOCUSED: DBG("FOCUSED"); return;
-		// case LV_EVENT_DEFOCUSED: DBG("DEFOCUSED"); return;
-		case LV_EVENT_LEAVE: DBG("LEAVE"); return;
-		// case LV_EVENT_HIT_TEST: DBG("HIT_TEST"); return;
+		case LV_EVENT_KEY: DBG("%s: KEY", info); return;
+		case LV_EVENT_FOCUSED: DBG("%s: FOCUSED", info); return;
+		// case LV_EVENT_DEFOCUSED: DBG("%s: DEFOCUSED", info); return;
+		case LV_EVENT_LEAVE: DBG("%s: LEAVE", info); return;
+		// case LV_EVENT_HIT_TEST: DBG("%s: HIT_TEST", info); return;
 
 		// Special events
-		case LV_EVENT_VALUE_CHANGED: DBG("VALUECHANGED"); return;
-		case LV_EVENT_INSERT: DBG("INSERT"); return;
-    	case LV_EVENT_REFRESH: DBG("REFRES"); return;
-    	case LV_EVENT_READY: DBG("READY"); return;
-    	case LV_EVENT_CANCEL: DBG("CANCEL"); return;
+		case LV_EVENT_VALUE_CHANGED: DBG("%s: VALUECHANGED", info); return;
+		case LV_EVENT_INSERT: DBG("%s: INSERT", info); return;
+    	case LV_EVENT_REFRESH: DBG("%s: REFRES", info); return;
+    	case LV_EVENT_READY: DBG("%s: READY", info); return;
+    	case LV_EVENT_CANCEL: DBG("%s: CANCEL", info); return;
 
     	// Other events
-    	case LV_EVENT_DELETE: DBG("DELETE"); return;
+    	case LV_EVENT_DELETE: DBG("%s: DELETE", info); return;
 
 		default: return;
 	};
@@ -43,19 +47,24 @@ void debug_event_drawing(lv_event_t * e)
 {
 	lv_event_code_t code = lv_event_get_code(e);
     // lv_obj_t * obj = lv_event_get_target(e);
+
+	const char* info = "";
+	if(e->user_data)
+		info = (const char*) e->user_data;
+
 	switch(code)
 	{
     /** Drawing events*/
-		case LV_EVENT_COVER_CHECK: DBG("COVER_CHECK"); return;
-		case LV_EVENT_REFR_EXT_DRAW_SIZE: DBG("EXT_DRAW_SIZE"); return;
-		case LV_EVENT_DRAW_MAIN_BEGIN: DBG("MAIN_BEGIN"); return;
-		case LV_EVENT_DRAW_MAIN        : DBG("MAIN"); return;
-		case LV_EVENT_DRAW_MAIN_END   : DBG("MAIN_END"); return;
-		case LV_EVENT_DRAW_POST_BEGIN  : DBG("POST_BEGIN"); return;
-		case LV_EVENT_DRAW_POST: DBG("POST"); return;
-		case LV_EVENT_DRAW_POST_END: DBG("POST_END"); return;
-		case LV_EVENT_DRAW_PART_BEGIN: DBG("PART_BEGIN"); return;
-		case LV_EVENT_DRAW_PART_END: DBG("PART_END"); return;
+		case LV_EVENT_COVER_CHECK: DBG("%s: COVER_CHECK", info); return;
+		case LV_EVENT_REFR_EXT_DRAW_SIZE: DBG("%s: EXT_DRAW_SIZE", info); return;
+		case LV_EVENT_DRAW_MAIN_BEGIN: DBG("%s: MAIN_BEGIN", info); return;
+		case LV_EVENT_DRAW_MAIN        : DBG("%s: MAIN", info); return;
+		case LV_EVENT_DRAW_MAIN_END   : DBG("%s: MAIN_END", info); return;
+		case LV_EVENT_DRAW_POST_BEGIN  : DBG("%s: POST_BEGIN", info); return;
+		case LV_EVENT_DRAW_POST: DBG("%s: POST", info); return;
+		case LV_EVENT_DRAW_POST_END: DBG("%s: POST_END", info); return;
+		case LV_EVENT_DRAW_PART_BEGIN: DBG("%s: PART_BEGIN", info); return;
+		case LV_EVENT_DRAW_PART_END: DBG("%s: PART_END", info); return;
 		default: return;
 	};
 };
@@ -64,14 +73,19 @@ void debug_event_special(lv_event_t * e)
 {
 	lv_event_code_t code = lv_event_get_code(e);
     // lv_obj_t * obj = lv_event_get_target(e);
+
+	const char* info = "";
+	if(e->user_data)
+		info = (const char*) e->user_data;
+
 	switch(code)
 	{
 		// Special events
-		case LV_EVENT_VALUE_CHANGED: DBG("VALUECHANGED"); return;
-		case LV_EVENT_INSERT: DBG("INSERT"); return;
-    	case LV_EVENT_REFRESH: DBG("REFRES"); return;
-    	case LV_EVENT_READY: DBG("READY"); return;
-    	case LV_EVENT_CANCEL: DBG("CANCEL"); return;
+		case LV_EVENT_VALUE_CHANGED: DBG("%s: VALUECHANGED", info); return;
+		case LV_EVENT_INSERT: DBG("%s: INSERT", info); return;
+    	case LV_EVENT_REFRESH: DBG("%s: REFRES", info); return;
+    	case LV_EVENT_READY: DBG("%s: READY", info); return;
+    	case LV_EVENT_CANCEL: DBG("%s: CANCEL", info); return;
 
 		default: return;
 	};
@@ -81,21 +95,26 @@ void debug_event_other(lv_event_t * e)
 {
 	lv_event_code_t code = lv_event_get_code(e);
     // lv_obj_t * obj = lv_event_get_target(e);
+
+	const char* info = "";
+	if(e->user_data)
+		info = (const char*) e->user_data;
+	
 	switch(code)
 	{
     	// Other events
-		case LV_EVENT_DELETE              : DBG("DELETE"); return;
-		case LV_EVENT_CHILD_CHANGED       : DBG("CHILD_CHANGED"); return;
-		case LV_EVENT_CHILD_CREATED       : DBG("CHILD_CREATED"); return;
-		case LV_EVENT_CHILD_DELETED       : DBG("CHILD_DETELED"); return;
-		case LV_EVENT_SCREEN_UNLOAD_START : DBG("SCREEN_UNLOAD_START"); return;
-		case LV_EVENT_SCREEN_LOAD_START   : DBG("LOAD_START"); return;
-		case LV_EVENT_SCREEN_LOADED       : DBG("LOADED"); return;
-		case LV_EVENT_SCREEN_UNLOADED     : DBG("UNLOADED"); return;
-		case LV_EVENT_SIZE_CHANGED        : DBG("SIZE_CHANGED"); return;
-		case LV_EVENT_STYLE_CHANGED       : DBG("STYLE_CHANGED"); return;
-		case LV_EVENT_LAYOUT_CHANGED      : DBG("LAYOUT_CHANGED"); return;
-		case LV_EVENT_GET_SELF_SIZE       : DBG("GET_SELF_SIZE"); return;
+		case LV_EVENT_DELETE              : DBG("%s: DELETE", info); return;
+		case LV_EVENT_CHILD_CHANGED       : DBG("%s: CHILD_CHANGED", info); return;
+		case LV_EVENT_CHILD_CREATED       : DBG("%s: CHILD_CREATED", info); return;
+		case LV_EVENT_CHILD_DELETED       : DBG("%s: CHILD_DETELED", info); return;
+		case LV_EVENT_SCREEN_UNLOAD_START : DBG("%s: SCREEN_UNLOAD_START", info); return;
+		case LV_EVENT_SCREEN_LOAD_START   : DBG("%s: LOAD_START", info); return;
+		case LV_EVENT_SCREEN_LOADED       : DBG("%s: LOADED", info); return;
+		case LV_EVENT_SCREEN_UNLOADED     : DBG("%s: UNLOADED", info); return;
+		case LV_EVENT_SIZE_CHANGED        : DBG("%s: SIZE_CHANGED", info); return;
+		case LV_EVENT_STYLE_CHANGED       : DBG("%s: STYLE_CHANGED", info); return;
+		case LV_EVENT_LAYOUT_CHANGED      : DBG("%s: LAYOUT_CHANGED", info); return;
+		case LV_EVENT_GET_SELF_SIZE       : DBG("%s: GET_SELF_SIZE", info); return;
 		default: return;
 	};
 };
--- src/soogh-debug.h
+++ src/soogh-debug.h
@@ -7,3 +7,5 @@ void debug_event_input(lv_event_t * e);
 void debug_event_drawing(lv_event_t * e);
 void debug_event_special(lv_event_t * e);
 void debug_event_other(lv_event_t * e);
+
+#define DEBUG_EVENT_INPUT(obj)  	lv_obj_add_event_cb(obj, debug_event_input, LV_EVENT_ALL, (void*)(__FUNCTION__));
