e03f67af08030e3ea214dc6d3479c631b721c182
Tijs van Roon <knifter@gmail.com>
2021-11-30 22:23:27 +0100

    colors

--- src/soogh-color.h
+++ src/soogh-color.h
@@ -4,12 +4,16 @@
 #include "soogh-lgfx.h"
 
 #define COLOR_BLACK			lv_color_black()
+#define COLOR_WHITE			lv_color_white()
 #define COLOR_GREY			lv_palette_main(LV_PALETTE_GREY)
 #define COLOR_RED			lv_palette_main(LV_PALETTE_RED)
 #define COLOR_ORANGE		lv_palette_main(LV_PALETTE_ORANGE)
 #define COLOR_GREEN			lv_palette_main(LV_PALETTE_GREEN)
+#define COLOR_PURPLE		lv_palette_main(LV_PALETTE_PURPLE)
 
 #define COLOR_DARK_GREEN	lv_palette_darken(LV_PALETTE_GREEN, 1)
+#define COLOR_LIGHT_GREEN	lv_palette_lighten(LV_PALETTE_GREEN, 1)
 #define COLOR_LIGHT_GREY	lv_palette_lighten(LV_PALETTE_GREY, 4)
+#define COLOR_LIGHT_PURPLE	lv_palette_lighten(LV_PALETTE_PURPLE, 3)
 
 #endif // __SOOGH_COLOR_H
