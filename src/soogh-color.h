2260e0d21417d743af69c7084f2bfb78a5596418
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2022-02-06 15:52:17 +0100

    colors

--- src/soogh-color.h
+++ src/soogh-color.h
@@ -4,12 +4,16 @@
 #include "soogh-lgfx.h"
 
 
-#define COLOR_BLACK			lv_color_black()
-#define COLOR_WHITE			lv_color_white()
+#define COLOR_BLACK				lv_color_black()
+#define COLOR_WHITE				lv_color_white()
 
 #define COLOR_RED			lv_palette_main(LV_PALETTE_RED)
+#define COLOR_RED_DARK(x)		lv_palette_darken (LV_PALETTE_RED, x)
+#define COLOR_RED_LIGHT(x)		lv_palette_lighten(LV_PALETTE_RED, x)
 #define COLOR_PINK			lv_palette_main(LV_PALETTE_PINK)
 #define COLOR_PURPLE		lv_palette_main(LV_PALETTE_PURPLE)
+#define COLOR_PURPLE_DARK(x)		lv_palette_darken (LV_PALETTE_PURPLE, x)
+#define COLOR_PURPLE_LIGHT(x)		lv_palette_lighten(LV_PALETTE_PURPLE, x)
 #define COLOR_DEEP_PURPLE	lv_palette_main(LV_PALETTE_DEEP_PURPLE)
 #define COLOR_INDIGO		lv_palette_main(LV_PALETTE_INDIGO)
 #define COLOR_BLUE			lv_palette_main(LV_PALETTE_BLUE)
@@ -17,6 +21,8 @@
 #define COLOR_CYAN			lv_palette_main(LV_PALETTE_CYAN)
 #define COLOR_TEAL			lv_palette_main(LV_PALETTE_TEAL)
 #define COLOR_GREEN			lv_palette_main(LV_PALETTE_GREEN)
+#define COLOR_GREEN_DARK(x)		lv_palette_darken(LV_PALETTE_GREEN, x)
+#define COLOR_GREEN_LIGHT(x)	lv_palette_lighten(LV_PALETTE_GREEN, x)
 #define COLOR_LIGHT_GREEN	lv_palette_main(LV_PALETTE_LIGHT_GREEN)
 #define COLOR_LIME			lv_palette_main(LV_PALETTE_LIME)
 #define COLOR_YELLOW		lv_palette_main(LV_PALETTE_YELLOW)
@@ -26,19 +32,7 @@
 #define COLOR_BROWN			lv_palette_main(LV_PALETTE_BROWN)
 #define COLOR_BLUE_GREY		lv_palette_main(LV_PALETTE_BLUE_GREY)
 #define COLOR_GREY			lv_palette_main(LV_PALETTE_GREY)
-
-#define COLOR_GREEN_DARK2	lv_palette_darken(LV_PALETTE_GREEN, 2)
-#define COLOR_GREEN_LIGHT2	lv_palette_lighten(LV_PALETTE_GREEN, 2)
-#define COLOR_RED_LIGHT2	lv_palette_lighten(LV_PALETTE_RED, 2)
-#define COLOR_PURPLE_LIGHT3	lv_palette_lighten(LV_PALETTE_PURPLE, 3)
-
-#define COLOR_GREY_DARK1	lv_palette_darken(LV_PALETTE_GREY, 1)
-#define COLOR_GREY_DARK2	lv_palette_darken(LV_PALETTE_GREY, 2)
-#define COLOR_GREY_DARK3	lv_palette_darken(LV_PALETTE_GREY, 3)
-#define COLOR_GREY_DARK4	lv_palette_darken(LV_PALETTE_GREY, 4)
-#define COLOR_GREY_LIGHT1	lv_palette_lighten(LV_PALETTE_GREY, 1)
-#define COLOR_GREY_LIGHT2	lv_palette_lighten(LV_PALETTE_GREY, 2)
-#define COLOR_GREY_LIGHT3	lv_palette_lighten(LV_PALETTE_GREY, 3)
-#define COLOR_GREY_LIGHT4	lv_palette_lighten(LV_PALETTE_GREY, 4)
+#define COLOR_GREY_DARK(x)		lv_palette_darken(LV_PALETTE_GREY, x)
+#define COLOR_GREY_LIGHT(x)		lv_palette_lighten(LV_PALETTE_GREY, x)
 
 #endif // __SOOGH_COLOR_H
