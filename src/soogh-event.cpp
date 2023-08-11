16405f4d5619df224f7a07e26016deedf9029985
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2023-08-11 17:18:58 +0200

    pressed

--- src/soogh-event.cpp
+++ src/soogh-event.cpp
@@ -15,6 +15,7 @@ const char* soogh_event_name(soogh_event_t e)
 	if(e & KEY_A)               strcat(buf, "A");               // 1
 	if(e & KEY_B)               strcat(buf, "B");               // 1
 	if(e & KEY_C)               strcat(buf, "C");               // 1
+    // if(e & KEYTOOL_PRESSED)     strcat(buf, "_PRESSED");        // 7
 	if(e & KEYTOOL_SHORT)       strcat(buf, "_SHORT");          // 6
 	if(e & KEYTOOL_LONG)        strcat(buf, "_LONG");           // 5
 	if(e & KEYTOOL_LONG_REPEAT) strcat(buf, "_LONG_REPEAT");    // 12
