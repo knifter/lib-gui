2f422e21180cc958c3cc63f1333773ed79820aeb
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2023-08-11 15:56:29 +0200

    Add long released

--- src/soogh-event.cpp
+++ src/soogh-event.cpp
@@ -9,14 +9,15 @@ const char* soogh_event_name(soogh_event_t e)
 		return "KEY_NONE";
 	if(e == KEY_RELEASED)
 		return "KEY_RELEASED";
-		
-	static char buf[32];
-	strcpy(buf, "KEY_");
-	if(e & KEY_A) strcat(buf, "A");
-	if(e & KEY_B) strcat(buf, "B");
-	if(e & KEY_C) strcat(buf, "C");
-	if(e & KEYTOOL_SHORT) strcat(buf, "_SHORT");
-	if(e & KEYTOOL_LONG) strcat(buf, "_LONG");
-	if(e & KEYTOOL_LONG_REPEAT) strcat(buf, "_LONG_REPEAT");
+
+    static char buf[sizeof("KEY_ABC_SHORT_LONG_REPEAT_RELEASED")+1];
+	strcpy(buf, "KEY_");                                        // 4
+	if(e & KEY_A)               strcat(buf, "A");               // 1
+	if(e & KEY_B)               strcat(buf, "B");               // 1
+	if(e & KEY_C)               strcat(buf, "C");               // 1
+	if(e & KEYTOOL_SHORT)       strcat(buf, "_SHORT");          // 6
+	if(e & KEYTOOL_LONG)        strcat(buf, "_LONG");           // 5
+	if(e & KEYTOOL_LONG_REPEAT) strcat(buf, "_LONG_REPEAT");    // 12
+    if(e & KEYTOOL_RELEASED)    strcat(buf, "_RELEASED");       // 9
 	return buf;
 };
